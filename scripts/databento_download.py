c5daa3a494d13da14c0927426e8d8b17b02fac28
Guy <guy.invest@icloud.com>
2025-09-22 17:37:26 +1000

    added data for various futures

--- scripts/databento_download.py
+++ scripts/databento_download.py
@@ -13,11 +13,12 @@ if not API_KEY:
 
 DATASET = "glbx.mdp3"
 SCHEMA = "ohlcv-1m"
-SYMBOL  = "MNQ.v.0"       # MNQ1! equivalent on TradingView
-START = "2025-09-01"
-END = "2025-09-21"
-OUTPUT_FILE = Path(f"data/mnq_{START}_{END}_1m.csv")
-RAW_OUTPUT_FILE = OUTPUT_FILE.with_name(f"mnq_{START}_{END}_1m_raw.csv")
+SYMBOL = "MES.v.0"       # ES1! equivalent on TradingView
+SYMBOL_SLUG = SYMBOL.lower()
+START = "2023-01-01"
+END = "2025-09-20"
+OUTPUT_FILE = Path(f"data/{SYMBOL_SLUG}_{START}_{END}_1m.csv")
+RAW_OUTPUT_FILE = OUTPUT_FILE.with_name(f"{SYMBOL_SLUG}_{START}_{END}_1m_raw.csv")
 
 
 def main() -> None:
